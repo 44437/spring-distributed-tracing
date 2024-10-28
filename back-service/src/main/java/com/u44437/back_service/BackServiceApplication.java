43933df78e2b3074e2a33a5b46e850ea8fa443e1
44437 <ercantopuz33@gmail.com>
2024-10-28 16:47:43 +0300

    Initial commit

---
+++ back-service/src/main/java/com/u44437/back_service/BackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.u44437.back_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackServiceApplication.class, args);
+	}
+
+}
---
+++ back-service/src/test/java/com/u44437/back_service/BackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u44437.back_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
